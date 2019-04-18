75444b999209474dbee1b5b3806741eac22862ce
Gabriel Sandu <allmediacreation@gmail.com>
2019-04-18 19:08:06 +0300

    Updates

---
+++ precache-manifest.947e876a51eb25312fac0ae070be2b3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c52ba3cc1479f468f64",
+    "url": "/React-Shop/static/js/runtime~main.ee72ac26.js"
+  },
+  {
+    "revision": "dc5956e4f2aef5f4916a",
+    "url": "/React-Shop/static/js/main.8ee1a96b.chunk.js"
+  },
+  {
+    "revision": "b52c03ac200cdc1be66e",
+    "url": "/React-Shop/static/js/2.a9e494ff.chunk.js"
+  },
+  {
+    "revision": "dc5956e4f2aef5f4916a",
+    "url": "/React-Shop/static/css/main.965582d2.chunk.css"
+  },
+  {
+    "revision": "4525cd0962d604096a1d1256d9292429",
+    "url": "/React-Shop/index.html"
+  }
+];
