46e142475f0c1bc4c96a735010d52d2307f6fdfa
Gabriel Sandu <allmediacreation@gmail.com>
2019-04-30 18:08:59 +0300

    Updates

--- precache-manifest.aa3352d8b7344e8b50a39c50d6f4e17c.js
+++ precache-manifest.aa3352d8b7344e8b50a39c50d6f4e17c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React-Shop/static/js/runtime~main.ee72ac26.js"
   },
   {
-    "revision": "48caa6e2e186813d5646",
-    "url": "/React-Shop/static/js/main.2ad7ca5b.chunk.js"
+    "revision": "955aac3201f8e5d9959d",
+    "url": "/React-Shop/static/js/main.bfe6ccae.chunk.js"
   },
   {
     "revision": "5eef2da52af7b202dad6",
     "url": "/React-Shop/static/js/2.fc42a64e.chunk.js"
   },
   {
-    "revision": "48caa6e2e186813d5646",
-    "url": "/React-Shop/static/css/main.838c8367.chunk.css"
+    "revision": "955aac3201f8e5d9959d",
+    "url": "/React-Shop/static/css/main.fb4802f5.chunk.css"
   },
   {
-    "revision": "a88637dbc3b1ae61e044974378b0f1e4",
+    "revision": "d3fe8918719f9f166f8e69be157a6483",
     "url": "/React-Shop/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Shop/precache-manifest.0ce6cdd671119cf771112df63fc7f46a.js"
+  "/React-Shop/precache-manifest.aa3352d8b7344e8b50a39c50d6f4e17c.js"
 );
 
 workbox.clientsClaim();
